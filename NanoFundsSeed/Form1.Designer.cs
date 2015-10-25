addd0770246ec4751015cdd409606da0131094c7
Adrienne Tacke <adrienne.tacke@gmail.com>
2015-10-24 20:20:02 -0700

    Initial commit.

---
+++ NanoFundsSeed/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace NanoFundsSeed
+{
+    partial class NanoFundsSeedDataGenerator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.datePicker = new System.Windows.Forms.DateTimePicker();
+            this.Generate = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // datePicker
+            // 
+            this.datePicker.Location = new System.Drawing.Point(12, 19);
+            this.datePicker.Name = "datePicker";
+            this.datePicker.Size = new System.Drawing.Size(200, 20);
+            this.datePicker.TabIndex = 0;
+            this.datePicker.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
+            // 
+            // Generate
+            // 
+            this.Generate.Location = new System.Drawing.Point(218, 16);
+            this.Generate.Name = "Generate";
+            this.Generate.Size = new System.Drawing.Size(75, 23);
+            this.Generate.TabIndex = 1;
+            this.Generate.Text = "Generate";
+            this.Generate.UseVisualStyleBackColor = true;
+            this.Generate.Click += new System.EventHandler(this.Generate_Click);
+            // 
+            // NanoFundsSeedDataGenerator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(364, 53);
+            this.Controls.Add(this.Generate);
+            this.Controls.Add(this.datePicker);
+            this.Name = "NanoFundsSeedDataGenerator";
+            this.Text = "Nano Funds Seed Data Generator";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DateTimePicker datePicker;
+        private System.Windows.Forms.Button Generate;
+    }
+}
+
---
+++ NanoFundsSeed/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace NanoFundsSeed
+{
+    public partial class NanoFundsSeedDataGenerator : Form
+    {
+        DateTime dateOfService = DateTime.Today;
+
+        public NanoFundsSeedDataGenerator()
+        {
+            InitializeComponent();
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+            dateOfService = datePicker.Value;
+        }
+
+        private void Generate_Click(object sender, EventArgs e)
+        {
+            var path = @"C:\Users\Adrienne\hackathon\NanoSeedData.txt";
+
+            var rnd = new Random();
+            var profit = 0;
+            
+
+            string[] lowDays = { "Monday", "Tuesday", "Wednesday", "Thursday" };
+            string[] highDays = { "Friday", "Saturday", "Sunday" };
+
+            if (!File.Exists(path))
+            {
+                File.Create(path).Dispose();
+                using (TextWriter tw = new StreamWriter(path))
+                {
+                    for (int day = 0; day <= 365; day++)
+                    {
+                        var dayOfTheWeek = dateOfService.DayOfWeek.ToString();
+
+                        if (lowDays.Any(dayOfTheWeek.Contains))
+                        {
+                            profit = rnd.Next(49, 101);
+                        }
+                        else if (highDays.Any(dayOfTheWeek.Contains))
+                        {
+                            profit = rnd.Next(100, 300);
+                        }
+
+                        tw.WriteLine(dayOfTheWeek + " " + dateOfService.ToShortDateString() + ": " + "$" + profit);
+                        dateOfService = dateOfService.AddDays(1);
+                    }
+                    tw.Close();
+                }
+
+                MessageBox.Show("Generated seed data!");
+            }
+        }
+    }
+}
