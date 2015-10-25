fa5603fc0ec50e391a04c573d2d1f5fa69876ccf
Adrienne Tacke <adrienne.tacke@gmail.com>
2015-10-24 22:22:38 -0700

    Adjust logic for generating sql script.

--- NanoFundsSeed/Form1.cs
+++ NanoFundsSeed/Form1.cs
@@ -19,25 +19,43 @@ namespace NanoFundsSeed
             dateOfService = datePicker.Value;
         }
 
+
         private void Generate_Click(object sender, EventArgs e)
         {
-            var path = @"C:\Users\Adrienne\hackathon\NanoSeedData.txt";
+            var path = @"C:\Users\Adrienne\hackathon\NanoSeedData.sql";
+
+            string[] merchantNames = { "The Stalk Market", "Specs Appeal", "Nothing Bundt Cakes" };
+            Random rndIndex = new Random();
+            int index = rndIndex.Next(0, merchantNames.Length);
+
+            Guid merchantId = Guid.NewGuid();
+
+            var merchantName = merchantNames[index];
+            var balance = 0.0;
 
             var rnd = new Random();
             var profit = 0;
             
-
             string[] lowDays = { "Monday", "Tuesday", "Wednesday", "Thursday" };
             string[] highDays = { "Friday", "Saturday", "Sunday" };
 
             if (!File.Exists(path))
             {
                 File.Create(path).Dispose();
+
                 using (TextWriter tw = new StreamWriter(path))
                 {
-                    for (int day = 0; day <= 365; day++)
+                    tw.WriteLine("USE [nanofunds]");
+
+                    string merchantScript = String.Format(@"INSERT INTO dbo.Merchants (Id, Name, Balance) VALUES ('{0}', '{1}', {2});", merchantId, merchantName, balance);
+                    tw.WriteLine(merchantScript);
+
+                    tw.WriteLine("INSERT INTO dbo.ActualReceipts (Id, Amount, Date, Merchant_Id) VALUES ");
+
+                    for (int day = 0; day <= 125; day++)
                     {
                         var dayOfTheWeek = dateOfService.DayOfWeek.ToString();
+                        Guid actualReceiptId = Guid.NewGuid();
 
                         if (lowDays.Any(dayOfTheWeek.Contains))
                         {
@@ -48,13 +66,24 @@ namespace NanoFundsSeed
                             profit = rnd.Next(100, 300);
                         }
 
-                        tw.WriteLine(dayOfTheWeek + " " + dateOfService.ToShortDateString() + ": " + "$" + profit);
-                        dateOfService = dateOfService.AddDays(1);
+                        string merchantDataScript = String.Format(@"('{0}', {1}, {2}, '{3}')", actualReceiptId, profit, dateOfService.ToShortDateString(), merchantId);
+
+
+                        if (day == 125)
+                        {
+                            tw.WriteLine(merchantDataScript + ";");
+                        }
+                        else
+                        {
+                            tw.WriteLine(merchantDataScript + ",");
+                        }
+
+                        dateOfService = dateOfService.AddDays(-1);
                     }
                     tw.Close();
                 }
 
-                MessageBox.Show("Generated seed data!");
+                MessageBox.Show("Generated seed script!");
             }
         }
     }
